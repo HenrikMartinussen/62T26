50617f5c6aa6bcc48c4a123cbfcd37c6d9e75c2f
Henrik Martinussen <henrik.martinussen@gmail.com>
2022-10-27 00:48:49 +0200

    Added Java Swing gui experiment to git

---
+++ src/main/java/java/cardgame/DeckImage.java
@@ -0,0 +1,38 @@
+package cardgame;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class DeckImage {
+
+
+    private final int SOURCE_WIDTH = 360;
+    private final int SOURCE_HEIGHT = 540;
+    private final int DESTINATION_WIDTH = 100;
+    private final int DESTINATION_HEIGHT = 147;
+    private final int offset = 30;
+    private final int margin = 30;
+    private static final String LOCATION = "/resources/cards.png";
+    private BufferedImage deckImage;
+
+    DeckImage() {
+        InputStream stream = getClass().getResourceAsStream(LOCATION);
+        try {
+            deckImage = ImageIO.read(stream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    BufferedImage getImage(int row, int column) {
+        int sourceX = offset + (column * (SOURCE_WIDTH + margin));
+        int sourceY = offset + (row    * (SOURCE_HEIGHT + margin));
+        BufferedImage image = new BufferedImage(DESTINATION_WIDTH, DESTINATION_HEIGHT, BufferedImage.TYPE_INT_RGB);
+        Graphics g = image.createGraphics();
+        g.drawImage(deckImage, 0, 0, DESTINATION_WIDTH, DESTINATION_HEIGHT, sourceX, sourceY, sourceX + SOURCE_WIDTH, sourceY + SOURCE_HEIGHT, null);
+        return image;
+    }
+}
